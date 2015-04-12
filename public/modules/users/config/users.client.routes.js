46b71caac691e920e66976fc38626cb8a5cb74d0
Andrew Nater <a.a.nater@gmail.com>
2015-04-11 20:43:42 -0400

    My Portfolio View Added
    - added user’s profile page
    - adjusted photo views to work like image “post”
    - several additions to users.css for profile page and related components
    - added “bio” text area to user edit page

--- public/modules/users/config/users.client.routes.js
+++ public/modules/users/config/users.client.routes.js
@@ -5,9 +5,13 @@ angular.module('users').config(['$stateProvider',
 	function($stateProvider) {
 		// Users state routing
 		$stateProvider.
+		state('edit profile', {
+			url: '/settings/edit',
+			templateUrl: 'modules/users/views/settings/edit-profile.client.view.html'
+		}).
 		state('profile', {
 			url: '/settings/profile',
-			templateUrl: 'modules/users/views/settings/edit-profile.client.view.html'
+			templateUrl: 'modules/users/views/settings/profile.client.view.html'
 		}).
 		state('skills', {
 			url: '/settings/skills',
